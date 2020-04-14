719ac649e1e467480c7585a9e1e8aac1fa7afe95
edphillips <edward.phillips47@gmail.com>
2020-04-14 16:14:14 +0100

    Added get_accs function to extract accuracy metric from loss values

--- utils.py
+++ utils.py
@@ -234,3 +234,10 @@ def create_weights(Jc, n_levels, clusts):
     return Wce, Wci
 
 
+def get_accs(losses, target_variance):
+    """function to get accuracy from an array of losses, using the variance of the target trace"""
+
+    accs = 100 * (1 - losses/target_variance)
+
+    return accs
+
