985101a13cdffa8977948a52d0383b5d7329b1fb
edphillips <edward.phillips47@gmail.com>
2020-05-04 09:23:18 +0100

    Same again but with 240s of data - want tight band for 1L boxplot, and higher accs all round

--- train_run.py
+++ train_run.py
@@ -20,10 +20,10 @@ def run():
             print(e)
 
     # Lets generate some inputs this time
-    E_spikes, I_spikes = gen_realistic_inputs(Tmax=100)
+    E_spikes, I_spikes = gen_realistic_inputs(Tmax=15000)
     #
-    X_e = spikes_to_input(E_spikes, Tmax=1600)
-    X_i = spikes_to_input(I_spikes, Tmax=1600)
+    X_e = spikes_to_input(E_spikes, Tmax=240000)
+    X_i = spikes_to_input(I_spikes, Tmax=240000)
     X_tot = np.vstack((X_e, X_i))
 
     # X_tot = tf.convert_to_tensor(np.load('Data/real_inputs.npy'), dtype=tf.float32)  # real inputs made earlier
@@ -63,7 +63,7 @@ def run():
     #                                                                               num_epochs=5000, learning_rate=0.001)
 
     # save data
-    np.savez_compressed('/scratch/eap40/val_tu_1n3', a=target_params_list, b=trained_params_list, c=inputs)
+    np.savez_compressed('/scratch/eap40/val_tu_1n4', a=target_params_list, b=trained_params_list, c=inputs)
 
     print("Procedure finished")
 
